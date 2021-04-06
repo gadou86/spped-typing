d8ad5218e8eaf4210e133b05f3b8ed21b41c414c
gad dayan <gadou86@gmail.com>
2021-04-05 23:19:28 -0400

    Starting typing game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [text, setText] = useState("")
+
+  function handleClick(event) {
+    const {value} = event.target
+    setText(value)
+  }
+  return(
+    <div>
+      <h1>How fast do you type</h1>
+      <textarea 
+        onChange={handleClick}
+      />
+      <h3>Time remaingin</h3>
+      <button>Start</button>
+      <h3>Word count</h3>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
